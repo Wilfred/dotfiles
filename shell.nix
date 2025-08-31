9be532eabac14b9b72997c384f3ab00e91c3e815
Wilfred Hughes <me@wilfred.me.uk>
2025-08-31 23:31:13 +0100

    Expand command list

--- shell.nix
+++ shell.nix
@@ -1,3 +1,7 @@
+# Start a session with these binaries on path with:
+#
+# $ nix-shell --run zsh
+
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
   pkgs = import nixpkgs { config = {}; overlays = []; };
@@ -5,7 +9,25 @@ in
 
 pkgs.mkShellNoCC {
   packages = with pkgs; [
+    # Fuzzy finding, essential for ctrl-r convenience.
     fzf
+    # Minimal editor
+    nano
+    # Like nano but with Emacs keybindings.
     zile
+    # Manipulate JSON.
+    jq
+    # Navigate large JSON files.
+    jless
+    # Count lines of code.
+    tokei
+    # Readable overview of mounts, including usage and filesytem (a
+    # nicer `df -h`).
+    duf
+    # Remembers previously visted directories and lets you switch
+    # between them.
+    zoxide
+    # Fast text search.
+    ripgrep
   ];
 }
